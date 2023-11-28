b46b3ec52de5e7999d346253002c23b7eed9af35
Ahmed Shehata <ahmedshehataq98@gmail.com>
2023-11-28 19:42:17 +0200

    added guard ( auth ) layer

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,16 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { userProviders } from 'src/providers/user.provider';
 import { DatabaseModule } from 'src/database/database.module';
-
+import { JwtModule } from '@nestjs/jwt';
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET_KEY,
+      global: true,
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
   providers: [UserService, ...userProviders],
   controllers: [UserController],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { Model } from 'mongoose';
 import { providerKeys } from 'src/constants/databaseModelsName';
 import { createUserDto, loginDto } from 'src/dtos/user.dto';
@@ -8,7 +9,12 @@ import { IUser } from 'src/interfaces/user.interface';
 export class UserService {
   constructor(
     @Inject(providerKeys.userProvider) private userService: Model<IUser>,
+    private jwtService: JwtService,
   ) {}
+
+  hashPassword(password: string): Promise<string> {
+    return;
+  }
 
   async createAccount(createUserDto: createUserDto) {
     // rest of code ...
