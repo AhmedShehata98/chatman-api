fad346cae871ce9db0b30249a6261b8ff579860d
Ahmed Shehata <ahmedshehataq98@gmail.com>
2023-11-28 20:07:36 +0200

    auth function was added
    - added login functionality
    - added signup functionality

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Body, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { createUserDto, loginDto } from 'src/dtos/user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  async login() {
-    return 'login user';
+  @Post('login')
+  async login(@Body() loginDto: loginDto) {
+    return await this.userService.login(loginDto);
   }
 
-  @Get()
-  async searchUsers() {
-    return 'search users';
+  @Post('signup')
+  async searchUsers(@Body() createUserDto: createUserDto) {
+    return await this.userService.createAccount(createUserDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,15 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Model } from 'mongoose';
 import { providerKeys } from 'src/constants/databaseModelsName';
 import { createUserDto, loginDto } from 'src/dtos/user.dto';
 import { IUser } from 'src/interfaces/user.interface';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -12,14 +18,52 @@ export class UserService {
     private jwtService: JwtService,
   ) {}
 
-  hashPassword(password: string): Promise<string> {
-    return;
+  async hashPassword(password: string): Promise<string> {
+    const hashedPassword = await bcrypt.hash(password, process.env.SALT_ROUNDS);
+    return hashedPassword;
+  }
+  async comparePassword({
+    hashedPassword,
+    password,
+  }: {
+    hashedPassword: string;
+    password: string;
+  }): Promise<boolean> {
+    const decodedPassword = await bcrypt.compare(password, hashedPassword);
+    return decodedPassword;
   }
 
-  async createAccount(createUserDto: createUserDto) {
-    // rest of code ...
+  async createAccount(
+    createUserDto: createUserDto,
+  ): Promise<{ user: IUser; token: string }> {
+    const hashedPassword = await this.hashPassword(createUserDto.password);
+    const user = await new this.userService({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+    await user.save();
+    const token = await this.jwtService.signAsync({ _id: user._id });
+    return { user, token };
   }
   async login(loginDto: loginDto) {
-    // rest of code ...
+    try {
+      const user = await this.userService.findOne({ email: loginDto.email });
+      if (!user) {
+        throw new NotFoundException();
+      }
+
+      const compareResult = await this.comparePassword({
+        hashedPassword: user.password,
+        password: loginDto.password,
+      });
+      if (compareResult) {
+        const token = await this.jwtService.sign({ _id: user._id });
+        return { message: `welcome back ,${user.username}`, token };
+      } else {
+        throw new UnauthorizedException();
+      }
+    } catch (error) {
+      throw new UnauthorizedException(error);
+    }
   }
 }
